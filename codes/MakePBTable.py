29327f515fbe39703616ce0211c4429c660ed0e2
hjkim88 <firadazer@gmail.com>
2019-09-12 18:12:01 -0400

    MakePBTable.py in implementation. load_all_players().

--- codes/MakePBTable.py
+++ codes/MakePBTable.py
@@ -27,8 +27,15 @@ def start():
 
 
 ### a function to get player IDs based on ROS files
+### e.g., dataPath="E:/HJ_Personal/Python/MLB_Analysis/data/regular/"
 def load_all_players(dataPath, start_year, end_year):
+    player_list = []
     for year in range(start_year, end_year+1):
-        with open(dataPath, 'r') as f:
-            teams = list(csv.reader(f))
-
+        with open(dataPath+"TEAM"+str(year), 'r') as f1:
+            teams = list(csv.reader(f1))
+        for team in teams:
+            with open(dataPath+team[0]+str(year)+".ROS", 'r') as f2:
+                players = list(csv.reader(f2))
+            player_list.extend(players)
+            ### get unique set...
+            player_list = [list(y) for y in set([tuple(x) for x in player_list])]
